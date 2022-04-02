95c21d9a15f6cd4f8027627966daf54f20caac96
PiSunWoo <ppssww1202@naver.com>
2022-04-02 17:09:56 +0900

    custom_nomalization

--- OAI-KL/test.py
+++ OAI-KL/test.py
@@ -7,8 +7,9 @@ from torchvision import models
 import torch.nn.functional as F
 from PIL import Image
 from dataset import ImageDataset
+import os
 
-test = pd.read_csv('./KneeXray/Test_cn_correct.csv')
+test = pd.read_csv('./KneeXray/Test_correct.csv')
 
 transform = transforms.Compose([ 
                                 transforms.ToTensor(),
@@ -18,7 +19,29 @@ transform = transforms.Compose([
 test_data = ImageDataset(test)
 testloader = DataLoader(test_data, batch_size=1, shuffle=False)
 
-fold = 5
+model_path = './models/'
+submission_path = './submission/'
+model_list = os.listdir(model_path)
+model_list_pt = [file for file in model_list if file.endswith(".pt")]
+
+for i in model_list_pt: 
+    model_ft = torch.load('{}{}'.format(model_path, i))
+    preds = []
+
+    for batch in testloader:
+        with torch.no_grad():
+            image = batch['image'].cuda()
+            output = model_ft(image)
+            preds.extend([i.item() for i in torch.argmax(output, axis = 1)])
+
+    submit = pd.DataFrame({'data':[i.split('/')[-1] for i in test['data']], 'label':preds})
+
+    fold_and_epoch = i[10:-3]
+    submit.to_csv('{}{}_submission.csv'.format(submission_path, fold_and_epoch))
+    print('save {}{}_submission.csv'.format(submission_path, fold_and_epoch))
+
+'''
+fold = 2
 epoch = 6
 
 model_ft = torch.load('./models/kfold_CNN_{}fold_epoch{}.pt'.format(fold, epoch))
@@ -33,3 +56,4 @@ for batch in testloader:
 submit = pd.DataFrame({'data':[i.split('/')[-1] for i in test['data']], 'label':preds})
 submit.to_csv('./submission/{}fold_epoch{}_submission.csv'.format(fold, epoch), index = False)
 print('saving submission.csv.....')
+'''
