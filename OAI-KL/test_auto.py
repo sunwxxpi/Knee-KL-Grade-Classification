f558017dbdba7bf46ee0467b4f121d7028f7416a
PiSunWoo <ppssww1202@naver.com>
2022-08-01 15:42:38 +0900

    Delete Remark

--- OAI-KL/test_auto.py
+++ OAI-KL/test_auto.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 from dataset import ImageDataset
 
-test_csv = pd.read_csv('./KneeXray/Test_correct.csv') # _cn _clahe 등, 수정 O, 수정 x -> 결과 비교
+test_csv = pd.read_csv('./KneeXray/Test_correct.csv')
 
 transform = transforms.Compose([
                                 transforms.ToTensor(),
@@ -39,14 +39,14 @@ for i in model_list_pt:
             image = batch['image'].cuda()
             target = batch['target'].cuda()
             output = model_ft(image)
-            preds.extend([i.item() for i in torch.argmax(output, axis=1)]) # tensor 자료형의 예측 라벨 값을 list로 뽑아 preds = []에 extend
+            preds.extend([i.item() for i in torch.argmax(output, axis=1)])
             
             softmax = nn.Softmax(dim=1)
             softmax_output = softmax(output).detach().cpu().numpy()
-            probs_correct.append(softmax_output[0][int(target)]) # softmax 출력값 중 correct label에 해당하는 확률값을 probs_correct = []에 append
-            probs_predict.append(max(softmax_output[0])) # softmax 출력값 중 predict label에 해당하는 확률값을 probs_predict = []에 append
+            probs_correct.append(softmax_output[0][int(target)])
+            probs_predict.append(max(softmax_output[0]))
             for k in range(5):
-                globals()['probs_{}'.format(k)].append(softmax_output[0][k]) # softmax 출력값 중 class에 해당하는 확률값을 probs_{} = []에 append
+                globals()['probs_{}'.format(k)].append(softmax_output[0][k])
         
     submit = pd.DataFrame({'data':[i.split('/')[-1] for i in test_csv['data']], 'label':preds, 'prob_correct':probs_correct, 'prob_predict':probs_predict, 'prob_0':probs_0, 'prob_1':probs_1, 'prob_2':probs_2, 'prob_3':probs_3, 'prob_4':probs_4})
 
