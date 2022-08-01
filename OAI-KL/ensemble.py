0b8405001372551b84973f6b5cf59936f2ab3fd0
PiSunWoo <ppssww1202@naver.com>
2022-08-01 15:43:26 +0900

    Add Soft Voting

---
+++ OAI-KL/ensemble.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import os
+import torch
+
+test_csv = pd.read_csv('./KneeXray/Test.csv')
+
+submission_path = './submission/'
+submission_list = os.listdir(submission_path)
+submission_list_csv = [file for file in submission_list if file.endswith(".csv")]
+
+for i in submission_list_csv:
+    globals()['{}_probs'.format(i)] = []
+    
+    for k in range(1656):
+        globals()['{}_image_{}'.format(i, k)] = [0, 0, 0, 0, 0]
+    
+    for k in range(5):
+        submission_csv = pd.read_csv('{}{}'.format(submission_path, i), names=['data', 'label', 'prob_correct', 'prob_predict', 'prob_0', 'prob_1', 'prob_2', 'prob_3', 'prob_4'], skiprows=1)
+        submission_probs = submission_csv['prob_{}'.format(k)]
+        submission_probs_list = submission_probs.values.tolist()
+        
+        for j in range(1656):
+            globals()['{}_image_{}'.format(i, j)][k] = submission_probs_list[j] 
+            
+    for k in range(1656):       
+        globals()['{}_probs'.format(i)].append(globals()['{}_image_{}'.format(i, k)])
+            
+ensemble = [[0 for j in range(5)] for i in range(1656)]
+preds = []
+
+for i in range(1656):
+    for k in range(5):
+        ensemble[i][k] = (globals()['{}_probs'.format('1fold_epoch14_submission.csv')][i][k] + globals()['{}_probs'.format('2fold_epoch17_submission.csv')][i][k]) / 2
+    
+    # ensemble[i][k] = (globals()['{}_probs'.format('1fold_epoch14_submission.csv')][i][k] + globals()['{}_probs'.format('2fold_epoch17_submission.csv')][i][k] + globals()['{}_probs'.format('3fold_epoch21_submission.csv')][i][k]) / 3 # 1.35 1.35 0.3 || 1.3 1.35 0.35 || 1.35 1.3 0.35
+
+ensemble_output = torch.tensor(ensemble)
+preds.extend([i.item() for i in torch.argmax(ensemble_output, axis=1)]) # tensor 자료형의 예측 라벨 값을 list로 뽑아 preds = []에 extend
+    
+submit = pd.DataFrame({'data':[i.split('/')[-1] for i in test_csv['data']], 'label':preds})
+
+submit.to_csv('{}ensemble_submission.csv'.format(submission_path), index=False)
+print('save ensemble_submission.csv')
