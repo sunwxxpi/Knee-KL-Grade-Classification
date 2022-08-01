16549471cfb1a531f4388fd81d041d9b1bdd73bf
PiSunWoo <ppssww1202@naver.com>
2022-08-01 15:50:17 +0900

    Add Soft Voting

--- OAI-KL/accuracy_score_auto.py
+++ OAI-KL/accuracy_score_auto.py
@@ -13,8 +13,8 @@ submission_list_csv = [file for file in submission_list if file.endswith(".csv")
 label = ['0', '1', '2', '3', '4'] # 라벨 설정
 
 for i in submission_list_csv:
-    csv = pd.read_csv('{}{}'.format(submission_path, i), names=['data', 'label', 'prob_correct', 'prob_predict', 'prob_0', 'prob_1', 'prob_2', 'prob_3', 'prob_4'])
-    submission_label = csv['label']
+    submission_csv = pd.read_csv('{}{}'.format(submission_path, i), names=['data', 'label', 'prob_correct', 'prob_predict', 'prob_0', 'prob_1', 'prob_2', 'prob_3', 'prob_4'])
+    submission_label = submission_csv['label']
     submission_label_list = submission_label.values.tolist()
     
     print('{}'.format(i))
