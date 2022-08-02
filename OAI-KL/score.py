94839261c727a9b34a6c926a157265079bfd2c42
PiSunWoo <ppssww1202@naver.com>
2022-08-02 23:03:27 +0900

    Retouch csv

--- OAI-KL/score.py
+++ OAI-KL/score.py
@@ -3,37 +3,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, roc_curve
 
-csv = pd.read_csv('./KneeXray/Test_correct.csv', names = ['data', 'label'])
-Test_correct_label = csv['label']
-Test_correct_label_list = Test_correct_label.values.tolist()
-Test_correct_label_list = Test_correct_label_list[1:]
+test_csv = pd.read_csv('./KneeXray/Test_correct.csv', names=['data', 'label'], skiprows=1)
+test_correct_labels = test_csv['label']
+test_correct_labels_list = test_correct_labels.values.tolist()
 
-fold = 2
-epoch = 7
+fold = 5
+epoch = 4
 
-csv = pd.read_csv('./submission/{}fold_epoch{}_submission.csv'.format(fold, epoch), names=['data', 'label', 'prob'])
-submission_label = csv['label']
-submission_label_list = submission_label.values.tolist()
-submission_label_list = submission_label_list[1:]
-submission_prob = csv['prob']
-submission_prob_list = submission_prob.values.tolist()
-submission_prob_list = submission_prob_list[1:]
+submission_csv = pd.read_csv('./submission/{}fold_epoch{}_submission.csv'.format(fold, epoch), names=['data', 'label', 'prob_correct', 'prob_predict', 'prob_0', 'prob_1', 'prob_2', 'prob_3', 'prob_4'], skiprows=1)
 
-label = ['0', '1', '2', '3', '4'] # 라벨 설정
+submission_labels = submission_csv['label']
+submission_labels_list = submission_labels.values.tolist()
+# submission_probs = csv['prob']
+# submission_probs_list = submission_probs.values.tolist()
+
 normalize = 'true'
-score = accuracy_score(Test_correct_label_list, submission_label_list)
-report = classification_report(Test_correct_label_list, submission_label_list, labels=label, digits=4) # micro avg f1 score = accuracy score
-matrix = confusion_matrix(Test_correct_label_list, submission_label_list, labels=label, normalize=normalize)
-
-for i in range(5):
-    fprs, tprs, _ = roc_curve(Test_correct_label_list[:,i], submission_prob_list[:,i]) #calculate fprs and tprs for each class
-    plt.plot(fprs,tprs,label='{}'.format(i)) #plot roc curve of each class
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.legend()
-
+score = accuracy_score(test_correct_labels_list, submission_labels_list)
+report = classification_report(test_correct_labels_list, submission_labels_list, digits=4) # micro avg f1 score = accuracy score
+matrix = confusion_matrix(test_correct_labels_list, submission_labels_list, normalize=normalize)
 print(report)
-                         
+
+# for i in range(5):
+#     fprs, tprs, _ = roc_curve(test_correct_labels_list[:,i], submission_prob_list[:,i]) #calculate fprs and tprs for each class
+#     plt.plot(fprs,tprs,label='{}'.format(i)) #plot roc curve of each class
+# plt.xlabel('False Positive Rate')
+# plt.ylabel('True Positive Rate')
+# plt.legend()
+       
 title = 'Confusion Matrix'
 plt.figure(figsize=(7, 7.5))
 plt.imshow(matrix, interpolation='nearest', cmap=plt.cm.Blues)
@@ -49,6 +45,6 @@ fmt = '.3f' if normalize else 'd'
 thresh = 200
 for i in range(matrix.shape[0]):
     for j in range(matrix.shape[1]):
-        plt.text(j, i, format(matrix[i, j], fmt), ha="center", va="center", color="white" if matrix[i, j] > thresh else "black")  # horizontal alignment
+        plt.text(j, i, format(matrix[i, j], fmt), ha="center", va="center", color="white" if matrix[i, j] > thresh else "black")  # Horizontal Alignment
 
 plt.show()
--- OAI-KL/score_auto.py
+++ OAI-KL/score_auto.py
@@ -2,22 +2,20 @@ import os
 import pandas as pd
 from sklearn.metrics import classification_report
 
-test_csv = pd.read_csv('./KneeXray/Test_correct.csv', names=['data', 'label'])
-Test_correct_labels = test_csv['label']
-Test_correct_labels_list = Test_correct_labels.values.tolist()
+test_csv = pd.read_csv('./KneeXray/Test_correct.csv', names=['data', 'label'], skiprows=1)
+test_correct_labels = test_csv['label']
+test_correct_labels_list = test_correct_labels.values.tolist()
 
 submission_path = './submission/'
 submission_list = os.listdir(submission_path)
 submission_list_csv = [file for file in submission_list if file.endswith(".csv")]
 
-label = ['0', '1', '2', '3', '4']
-
 for i in submission_list_csv:
-    submission_csv = pd.read_csv('{}{}'.format(submission_path, i), names=['data', 'label', 'prob_correct', 'prob_predict', 'prob_0', 'prob_1', 'prob_2', 'prob_3', 'prob_4'])
-    submission_label = submission_csv['label']
-    submission_label_list = submission_label.values.tolist()
+    submission_csv = pd.read_csv('{}{}'.format(submission_path, i), names=['data', 'label', 'prob_correct', 'prob_predict', 'prob_0', 'prob_1', 'prob_2', 'prob_3', 'prob_4'], skiprows=1)
+    submission_labels = submission_csv['label']
+    submission_labels_list = submission_labels.values.tolist()
     
     print('{}'.format(i))
-    report = classification_report(Test_correct_labels_list, submission_label_list, labels=label, digits=4)
+    report = classification_report(test_correct_labels_list, submission_labels_list, digits=4)
     print(report)
     print()
