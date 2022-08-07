875343e66f9b2a659ea96550a120bd621a5a447b
PiSunWoo <ppssww1202@naver.com>
2022-08-07 15:40:56 +0900

    Add Library natsort

--- OAI-KL/score_auto.py
+++ OAI-KL/score_auto.py
@@ -1,4 +1,5 @@
 import os
+import natsort
 import pandas as pd
 from sklearn.metrics import classification_report
 
@@ -9,6 +10,7 @@ test_correct_labels_list = test_correct_labels.values.tolist()
 submission_path = './submission/'
 submission_list = os.listdir(submission_path)
 submission_list_csv = [file for file in submission_list if file.endswith(".csv")]
+submission_list_csv = natsort.natsorted(submission_list_csv)
 
 for i in submission_list_csv:
     submission_csv = pd.read_csv('{}{}'.format(submission_path, i), names=['data', 'label', 'prob_correct', 'prob_predict', 'prob_0', 'prob_1', 'prob_2', 'prob_3', 'prob_4'], skiprows=1)
