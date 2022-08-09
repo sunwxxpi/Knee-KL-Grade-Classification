ec0c1f590b208725c34451d9eca7cc559255a9e7
PiSunWoo <ppssww1202@naver.com>
2022-08-09 17:26:34 +0900

    Add Probs in CSV

--- OAI-KL/test_ensemble.py
+++ OAI-KL/test_ensemble.py
@@ -1,8 +1,11 @@
 import os
 import pandas as pd
+import numpy as np
 import torch
 
-test_csv = pd.read_csv('./KneeXray/Test.csv')
+test_csv = pd.read_csv('./KneeXray/Test_correct.csv', names=['data', 'label'], skiprows=1)
+test_correct_labels = test_csv['label']
+test_correct_labels_list = test_correct_labels.values.tolist()
 
 submission_path = './submission/'
 submission_list = os.listdir(submission_path)
@@ -31,7 +34,7 @@ for i in submission_list_csv:
         submission_probs_list = submission_probs.values.tolist()
         
         for k in range(1656):
-            globals()['{}_image_{}'.format(i, k)][j] = submission_probs_list[k] 
+            globals()['{}_image_{}'.format(i, k)][j] = submission_probs_list[k]
             
     for j in range(1656):
         globals()['{}_probs'.format(i)].append(globals()['{}_image_{}'.format(i, j)])
@@ -41,31 +44,60 @@ mode = 'hard_voting'
 
 preds = []
 
-if mode == 'soft_voting':
-    for i in range(1656):
-        for j in range(5):
-            # probs_ensemble[i][j] = (globals()['{}_probs'.format('1fold_epoch14_submission.csv')][i][j] + globals()['{}_probs'.format('2fold_epoch17_submission.csv')][i][j]) / 2
+for i in range(1656):
+    for j in range(5):
+        # probs_ensemble[i][j] = (globals()['{}_probs'.format('2fold_epoch7_submission.csv')][i][j] + globals()['{}_probs'.format('5fold_epoch23_submission.csv')][i][j]) / 2
             
-            probs_ensemble[i][j] = (globals()['{}_probs'.format('1fold_epoch14_submission.csv')][i][j] + globals()['{}_probs'.format('3fold_epoch5_submission.csv')][i][j] + globals()['{}_probs'.format('4fold_epoch51_submission.csv')][i][j]) / 3 # 1.35 1.35 0.3 || 1.3 1.35 0.35 || 1.35 1.3 0.35
-    
+        probs_ensemble[i][j] = (globals()['{}_probs'.format('2fold_epoch7_submission.csv')][i][j] + globals()['{}_probs'.format('2fold_epoch17_submission.csv')][i][j] + globals()['{}_probs'.format('5fold_epoch23_submission.csv')][i][j]) / 3
+
+if mode == 'soft_voting':
     ensemble_output = torch.tensor(probs_ensemble)
     preds.extend([i.item() for i in torch.argmax(ensemble_output, axis=1)])
             
 elif mode == 'hard_voting':
     for i in range(1656):
         if len(labels_ensemble[i]) == len(set(labels_ensemble[i])):
-            for j in range(5):
-                # probs_ensemble[i][j] = (globals()['{}_probs'.format('1fold_epoch14_submission.csv')][i][j] + globals()['{}_probs'.format('2fold_epoch17_submission.csv')][i][j]) / 2
-            
-                probs_ensemble[i][j] = (globals()['{}_probs'.format('1fold_epoch14_submission.csv')][i][j] + globals()['{}_probs'.format('3fold_epoch5_submission.csv')][i][j] + globals()['{}_probs'.format('4fold_epoch51_submission.csv')][i][j]) / 3 # 1.35 1.35 0.3 || 1.3 1.35 0.35 || 1.35 1.3 0.35
-            
             probs_ensemble_output = torch.tensor([probs_ensemble[i]])
             preds.extend([j.item() for j in torch.argmax(probs_ensemble_output, axis=1)])
         
         else:
             preds.append(max(set(labels_ensemble[i]), key=labels_ensemble[i].count))
+
+probs_ensemble = np.array(probs_ensemble)
+probs_0 = probs_ensemble[:, 0]
+probs_1 = probs_ensemble[:, 1]
+probs_2 = probs_ensemble[:, 2]
+probs_3 = probs_ensemble[:, 3]
+probs_4 = probs_ensemble[:, 4]
+
+probs_correct = [0 for i in range(1656)]
+probs_predict = [0 for i in range(1656)]
             
-submit = pd.DataFrame({'data':[i.split('/')[-1] for i in test_csv['data']], 'label':preds})
+for i in range(1656):
+    if test_correct_labels_list[i] == 0:
+        probs_correct[i] = globals()['probs_{}'.format('0')][i]
+    elif test_correct_labels_list[i] == 1:
+        probs_correct[i] = globals()['probs_{}'.format('1')][i]
+    elif test_correct_labels_list[i] == 2:
+        probs_correct[i] = globals()['probs_{}'.format('2')][i]
+    elif test_correct_labels_list[i] == 3:
+        probs_correct[i] = globals()['probs_{}'.format('3')][i]
+    elif test_correct_labels_list[i] == 4:
+        probs_correct[i] = globals()['probs_{}'.format('4')][i]
+        
+for i in range(1656):
+    if preds[i] == 0:
+        probs_predict[i] = globals()['probs_{}'.format('0')][i]
+    elif preds[i] == 1:
+        probs_predict[i] = globals()['probs_{}'.format('1')][i]
+    elif preds[i] == 2:
+        probs_predict[i] = globals()['probs_{}'.format('2')][i]
+    elif preds[i] == 3:
+        probs_predict[i] = globals()['probs_{}'.format('3')][i]
+    elif preds[i] == 4:
+        probs_predict[i] = globals()['probs_{}'.format('4')][i]
+        
+submit = pd.DataFrame({'data':[i.split('/')[-1] for i in test_csv['data']], 'label':preds, 'prob_correct':probs_correct, 'prob_predict':probs_predict, 'prob_0':probs_0, 'prob_1':probs_1, 'prob_2':probs_2, 'prob_3':probs_3, 'prob_4':probs_4})
 
 submit.to_csv('{}10fold_epoch10_submission.csv'.format(submission_path), index=False)
 print('save ensemble_submission.csv')
