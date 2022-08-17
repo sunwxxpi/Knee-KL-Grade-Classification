0004343bc09d44a4296f2478ae04fc666b105eeb
PiSunWoo <ppssww1202@naver.com>
2022-08-17 20:16:11 +0900

    Add Calculate Average

--- OAI-KL/score_auto.py
+++ OAI-KL/score_auto.py
@@ -12,6 +12,10 @@ submission_list = os.listdir(submission_path)
 submission_list_csv = [file for file in submission_list if file.endswith(".csv")]
 submission_list_csv = natsort.natsorted(submission_list_csv)
 
+sum_accuracy = 0
+sum_f1_macro = 0
+sum_f1_weighted = 0
+
 for i in submission_list_csv:
     submission_csv = pd.read_csv('{}{}'.format(submission_path, i), names=['data', 'label', 'prob_correct', 'prob_predict', 'prob_0', 'prob_1', 'prob_2', 'prob_3', 'prob_4'], skiprows=1)
     submission_labels = submission_csv['label']
@@ -22,8 +26,21 @@ for i in submission_list_csv:
     f1_macro = f1_score(test_correct_labels_list, submission_labels_list, average='macro')
     f1_weighted = f1_score(test_correct_labels_list, submission_labels_list, average='weighted')
     
-    if accuracy > 0.7 and f1_macro > 0.7 and f1_weighted > 0.7:
+    sum_accuracy += accuracy
+    sum_f1_macro += f1_macro
+    sum_f1_weighted += f1_weighted
+    
+    if accuracy > 0.66 and f1_macro > 0.66 and f1_weighted > 0.66:
         print('Accuracy Score : {}'.format(accuracy))
         print('F1 Score (Macro) : {}'.format(f1_macro))
         print('F1 Score (Weighted) : {}'.format(f1_weighted))
         print()
+
+avg_accuracy = sum_accuracy / 5.0
+avg_f1_macro = sum_f1_macro / 5.0
+avg_f1_weighted = sum_f1_weighted / 5.0
+
+print('Average Accuracy Score : {}'.format(avg_accuracy))
+print('Average F1 Score (Macro) : {}'.format(avg_f1_macro))
+print('Average F1 Score (Weighted) : {}'.format(avg_f1_weighted))
+print()
