896e04521f5f4de39c8c96fb8976a58335b26dc2
PiSunWoo <ppssww1202@naver.com>
2022-08-29 21:35:59 +0900

    Make HealthHub Dataset CSV

--- OAI-KL/make_csv.py
+++ OAI-KL/make_csv.py
@@ -1,39 +1,46 @@
 import glob
 import pandas as pd
 
-a = glob.glob('./KneeXray/train/0_cn/'+'*.png') # glob.glob >> list 형식으로 반환
-a_label = ['0' for x in range(0,len(a),1)]
-b = glob.glob('./KneeXray/train/1_cn/'+'*.png')
-b_label = ['1' for x in range(0,len(b),1)]
-c = glob.glob('./KneeXray/train/2_cn/'+'*.png')
-c_label = ['2' for x in range(0,len(c),1)]
-d = glob.glob('./KneeXray/train/3_cn/'+'*.png')
-d_label = ['3' for x in range(0,len(d),1)]
-e = glob.glob('./KneeXray/train/4_cn/'+'*.png')
-e_label = ['4' for x in range(0,len(e),1)]
+a = glob.glob('./KneeXray/HealthHub Dataset/train/0/'+'*.jpg') # glob.glob >> list 형식으로 반환
+a_label = ['0' for x in range(0, len(a), 1)]
+b = glob.glob('./KneeXray/HealthHub Dataset/train/1/'+'*.jpg')
+b_label = ['1' for x in range(0, len(b), 1)]
+c = glob.glob('./KneeXray/HealthHub Dataset/train/2/'+'*.jpg')
+c_label = ['2' for x in range(0, len(c), 1)]
+d = glob.glob('./KneeXray/HealthHub Dataset/train/3/'+'*.jpg')
+d_label = ['3' for x in range(0, len(d), 1)]
+e = glob.glob('./KneeXray/HealthHub Dataset/train/4/'+'*.jpg')
+e_label = ['4' for x in range(0, len(e), 1)]
 
 file = a + b + c + d + e
 label = a_label + b_label + c_label + d_label + e_label
 
 a = pd.DataFrame({'data':file, 'label':label})
 a.set_index('data', inplace=True) # 'data' 열을 index로 설정 EXCEL A열로 붙임
-a.to_csv('./KneeXray/Train_cn.csv')
+a.to_csv('./KneeXray/HealthHub Dataset/Train_HealthHub.csv')
 
-
-a = glob.glob('./KneeXray/test/0_cn/'+'*.png')
-a_label = ['0' for x in range(0,len(a),1)]
-b = glob.glob('./KneeXray/test/1_cn/'+'*.png')
-b_label = ['1' for x in range(0,len(b),1)]
-c = glob.glob('./KneeXray/test/2_cn/'+'*.png')
-c_label = ['2' for x in range(0,len(c),1)]
-d = glob.glob('./KneeXray/test/3_cn/'+'*.png')
-d_label = ['3' for x in range(0,len(d),1)]
-e = glob.glob('./KneeXray/test/4_cn/'+'*.png')
-e_label = ['4' for x in range(0,len(e),1)]
+a = glob.glob('./KneeXray/HealthHub Dataset/test/0/'+'*.jpg')
+a_label = ['0' for x in range(0, len(a), 1)]
+b = glob.glob('./KneeXray/HealthHub Dataset/test/1/'+'*.jpg')
+b_label = ['1' for x in range(0, len(b), 1)]
+c = glob.glob('./KneeXray/HealthHub Dataset/test/2/'+'*.jpg')
+c_label = ['2' for x in range(0, len(c), 1)]
+d = glob.glob('./KneeXray/HealthHub Dataset/test/3/'+'*.jpg')
+d_label = ['3' for x in range(0, len(d), 1)]
+e = glob.glob('./KneeXray/HealthHub Dataset/test/4/'+'*.jpg')
+e_label = ['4' for x in range(0, len(e), 1)]
 
 file = a + b + c + d + e
 label = a_label + b_label + c_label + d_label + e_label
 
 a = pd.DataFrame({'data':file, 'label':label})
 a. set_index('data', inplace=True)
-a.to_csv('./KneeXray/Test_cn_correct.csv')
+a.to_csv('./KneeXray/HealthHub Dataset/Test_correct_HealthHub.csv')
+
+""" 
+63 : 42 / 21
+198 : 132 / 66
+138 : 92 / 46
+83 : 55 / 28
+42 : 28 / 14
+"""
