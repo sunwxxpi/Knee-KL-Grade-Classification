b5defb0d92bd9d07983f58665c3b0f42dd73c77b
Pi Sun Woo <ppssww1202@naver.com>
2023-02-21 16:23:26 +0900

    Last Commit

--- OAI-KL/model.py
+++ OAI-KL/model.py
@@ -7,6 +7,16 @@ def model_return(args):
         in_ftrs = model_ft.fc.in_features
         model_ft.fc = nn.Linear(in_ftrs, 5)
                 
+    elif args.model_type == 'resnext_50_32x4d':
+        model_ft = models.resnext50_32x4d(weights='DEFAULT')
+        in_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(in_ftrs, 5)
+        
+    elif args.model_type == 'wide_resnet_50_2':
+        model_ft = models.wide_resnet50_2(weights='DEFAULT')
+        in_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(in_ftrs, 5)
+        
     elif args.model_type == 'densenet_169':
         model_ft = models.densenet169(weights='DEFAULT')
         in_ftrs = model_ft.classifier.in_features
@@ -32,23 +42,18 @@ def model_return(args):
         in_ftrs = model_ft.classifier[1].in_features
         model_ft.classifier[1] = nn.Linear(in_ftrs, 5)
 
-    elif args.model_type == 'resnext':
-        model_ft = models.resnext50_32x4d(weights='DEFAULT')
+    elif args.model_type == 'regnet_y_8gf':
+        model_ft = models.regnet_y_8gf(weights='DEFAULT')
         in_ftrs = model_ft.fc.in_features
         model_ft.fc = nn.Linear(in_ftrs, 5)
         
-    elif args.model_type == 'regnet':
-        model_ft = models.regnet_y_8gf(weights='DEFAULT')
+    elif args.model_type == 'shufflenet_v2_x2_0':
+        model_ft = models.shufflenet_v2_x2_0(weights='DEFAULT')
         in_ftrs = model_ft.fc.in_features
         model_ft.fc = nn.Linear(in_ftrs, 5)
         
     elif args.model_type == 'inception_v3':
         model_ft = models.inception_v3(weights='DEFAULT')
-        in_ftrs = model_ft.fc.in_features
-        model_ft.fc = nn.Linear(in_ftrs, 5)
-        
-    elif args.model_type == 'shufflenet_v2':
-        model_ft = models.shufflenet_v2_x2_0(weights='DEFAULT')
         in_ftrs = model_ft.fc.in_features
         model_ft.fc = nn.Linear(in_ftrs, 5)
         
