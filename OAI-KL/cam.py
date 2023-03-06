2e7cd6ff74b38c9d0f41ab3ec9bd0a6a05b2b186
Pi Sun Woo <ppssww1202@naver.com>
2023-03-06 17:17:55 +0900

    Best Ensemble Model CAM

--- OAI-KL/cam.py
+++ OAI-KL/cam.py
@@ -1,51 +1,74 @@
+import argparse
 import torch
 import torchvision
 import numpy as np
 import pandas as pd
-from torch import nn
-from torchvision import models
 from pytorch_grad_cam import AblationCAM, EigenCAM, FullGrad, GradCAM, GradCAMPlusPlus, ScoreCAM, XGradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 from PIL import Image
+from model import model_return
 
-""" model_ft = models.densenet201()
-in_ftrs = model_ft.classifier.in_features
-model_ft.classifier = nn.Linear(in_ftrs, 5) """
+parser = argparse.ArgumentParser()
+parser.add_argument('-m', '--model_type', dest='model_type', action='store')
+args = parser.parse_args()
 
-# model_ft = models.efficientnet_b5()
-model_ft = models.efficientnet_v2_s()
-in_ftrs = model_ft.classifier._modules.__getitem__('1').__getattribute__('in_features')
-sequential_0 = model_ft.classifier._modules.get('0')
-sequential_1 = nn.Linear(in_ftrs, 5)
-model_ft.classifier = nn.Sequential(sequential_0, sequential_1)
+model_ft = model_return(args)
+if args.model_type == 'densenet_161':
+    model_ft.load_state_dict(torch.load('./models/1_DenseNet-161.pt'))
+    target_layers = [model_ft.features[-1]]
+    image_size_tuple = (456, 456)
+    
+elif args.model_type == 'efficientnet_b5':
+    model_ft.load_state_dict(torch.load('./models/2_EfficientNet-b5.pt'))
+    target_layers = [model_ft.features[-1]]
+    image_size_tuple = (456, 456)
+    
+elif args.model_type == 'efficientnet_v2_s':
+    model_ft.load_state_dict(torch.load('./models/3_EfficientNet-V2-s.pt'))
+    target_layers = [model_ft.features[-1]]
+    image_size_tuple = (384, 384)
+    
+elif args.model_type == 'regnet_y_8gf':
+    model_ft.load_state_dict(torch.load('./models/4_RegNet-Y-8GF.pt'))
+    target_layers = [model_ft.trunk_output[-1]]
+    image_size_tuple = (448, 448)
+    
+elif args.model_type == 'resnet_101':
+    model_ft.load_state_dict(torch.load('./models/5_ResNet-101.pt'))
+    target_layers = [model_ft.layer4[-1]]
+    image_size_tuple = (456, 456)
+    
+elif args.model_type == 'resnext_50_32x4d':
+    model_ft.load_state_dict(torch.load('./models/6_ResNext-50-32x4d.pt'))
+    target_layers = [model_ft.layer4[-1]]
+    image_size_tuple = (512, 512)
+    
+elif args.model_type == 'wide_resnet_50_2':
+    model_ft.load_state_dict(torch.load('./models/7_Wide-ResNet-50-2.pt'))
+    target_layers = [model_ft.layer4[-1]]
+    image_size_tuple = (456, 456)
+    
+elif args.model_type == 'shufflenet_v2_x2_0':
+    model_ft.load_state_dict(torch.load('./models/8_ShuffleNet-V2-X2-0.pt'))
+    target_layers = [model_ft.conv5]    
+    image_size_tuple = (512, 512)
 
-# model_ft = torch.load('./models/DenseNet-201/models_448_DenseNet-201_32_0.0006/kfold_CNN_1fold_epoch18.pt') # DenseNet-201 Best
-# model_ft.load_state_dict(torch.load('./models/kfold_CNN_2fold_epoch26.pt')) # DenseNet-201 CAM
-# model_ft.load_state_dict(torch.load('./models/EfficientNet-b5/models_456_EfficientNet-b5_16_0.0005/kfold_CNN_5fold_epoch13.pt')) # EfficientNet-b5 Best
-# model_ft.load_state_dict(torch.load('./models/kfold_CNN_2fold_epoch4.pt')) # EfficientNet-b5 CAM
-model_ft.load_state_dict(torch.load('./models/EfficientNet-V2-s/models_384_EfficientNet-V2-s_16_0.0007/kfold_CNN_5fold_epoch35.pt')) # EfficientNet-V2-s Best & CAM
 model_ft.eval()
 
-# target_layers = [model_ft.module.features[-1]]
-target_layers = [model_ft.features[-1]]
-
-# cam = GradCAM(model=model_ft, target_layers=target_layers, use_cuda=True)
+cam = GradCAM(model=model_ft, target_layers=target_layers, use_cuda=True)
 # cam = GradCAMPlusPlus(model=model_ft, target_layers=target_layers, use_cuda=True)
-cam = ScoreCAM(model=model_ft, target_layers=target_layers, use_cuda=True)
+# cam = XGradCAM(model=model_ft, target_layers=target_layers, use_cuda=True)
+# cam = AblationCAM(model=model_ft, target_layers=target_layers, use_cuda=True)
+# cam = ScoreCAM(model=model_ft, target_layers=target_layers, use_cuda=True)
 
-test_csv = pd.read_csv('./KneeXray/Test_correct.csv', names=['data', 'label'], skiprows=1)
+test_csv = pd.read_csv(f'./KneeXray/Test_correct_{image_size_tuple}.csv', names=['data', 'label'], skiprows=1)
 test_img = test_csv['data']
 test_img_list = test_img.values.tolist()
-
-# path = ['./KneeXray/test/0/9850534R.png']
 
 for i in test_img_list:
     # Note: input_tensor can be a batch tensor with several images! """
     image = Image.open(i).convert('RGB')
-    # image = image.resize((448, 448))
-    # image = image.resize((456, 456))
-    image = image.resize((384, 384))
     image = (image - np.min(image)) / (np.max(image) - np.min(image)) # Max Min normalization
     convert_tensor = torchvision.transforms.ToTensor()
     input_tensor = convert_tensor(image).unsqueeze(0).float()
@@ -56,11 +79,11 @@ for i in test_img_list:
     # targets = [ClassifierOutputTarget(281)]
     # target_category = None
 
-    grayscale_cam = cam(input_tensor=input_tensor, aug_smooth=True, eigen_smooth=False)
+    grayscale_cam = cam(input_tensor=input_tensor, aug_smooth=True, eigen_smooth=True)
     grayscale_cam = grayscale_cam[0, :] # In this example grayscale_cam has only one image in the batch:
     
     visualization = show_cam_on_image(image, grayscale_cam, use_rgb=True)
 
     cam_img = Image.fromarray(visualization, 'RGB')
     cam_img = cam_img.resize((224, 224))
-    cam_img.save(f"./Grad CAM/{i.split('test')[-1]}")
+    cam_img.save(f"./Grad CAM/{args.model_type}/{i.split(f'test_{image_size_tuple}/')[-1]}")
